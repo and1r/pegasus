3fb956e47efef1e3bbfb428541381ba52ae4b0b7
and1r <111187173+and1r@users.noreply.github.com>
2025-12-16 22:46:13 +0100

    Add files via upload

--- script_v2.js
+++ script_v2.js
@@ -1,4 +1,4 @@
-alert("ÚJ JS FUT");
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, set, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
